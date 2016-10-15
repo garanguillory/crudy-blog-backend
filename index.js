84566d91cd1e7f04e77b8a6a8dfd6d1954976d34
Garan Guillory <garanguillory@gmail.com>
2016-10-15 14:09:55 -0500

    setting up the backend for the crudy-blog

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+// const cors = require('cors');
+
+
+
+// App
+app.use(morgan('combined'));
+// app.use(cors());
+app.use(bodyParser.json({type: '*/*'}));
+router(app);
+
+
+
+
+
+
+// Server
+
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on port Andre', port);
