bedd398b7d9c15a6299bf106f674fa9d86005a6e
Garan Guillory <garanguillory@gmail.com>
2016-11-15 18:52:26 -0600

    fixing router issues

--- router.js
+++ router.js
@@ -1,18 +1,18 @@
-const Authentication = require('./controllers/authentication');
-const passportService = require('./services/passport');
-const passport = require('passport');
-// const User = require('./models/user');
+// const Authentication = require('./controllers/authentication');
+// const passportService = require('./services/passport');
+// const passport = require('passport');
+// // const User = require('./models/user');
 
-const requireAuth = passport.authenticate('jwt', { session: false });
-const requireLogin = passport.authenticate('local', { session: false });
+// const requireAuth = passport.authenticate('jwt', { session: false });
+// const requireLogin = passport.authenticate('local', { session: false });
 
 
-module.exports = function(app){
-  app.get('/', function(req, res, next){
-    res.send(['one', 'two', 'thre']);
-  });
+// module.exports = function(app){
+//   app.get('/', function(req, res, next){
+//     res.send(['one', 'two', 'thre']);
+//   });
 
-  app.post('/login', requireLogin, Authentication.login);
-  app.post('/signup', Authentication.signup);
+//   app.post('/login', requireLogin, Authentication.login);
+//   app.post('/signup', Authentication.signup);
 
-}
+// }
